b22769befdd927ed24b641d372fe420a18cbe698
jimmyblack0112 <114201430+jimmyblack0112@users.noreply.github.com>
2024-03-07 17:04:39 +0800

    week03

---
+++ week03_mouse_glRotatef/main.cpp
@@ -0,0 +1,34 @@
+#include <GL/glut.h>
+#include <stdio.h>///為了printf()
+float teapotX = 0, teapotY = 0;
+float angle = 0;
+void mouse(int button, int state, int x, int y)
+{
+    teapotX = (x-150)/150.0;
+    teapotY = -(y-150)/150.0;
+}
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    glPushMatrix();
+        glRotatef(angle, 0, 0, 1);///對z軸轉
+        glutSolidTeapot(0.3);
+    glPopMatrix();
+    glutSwapBuffers();
+}
+void motion(int x, int y)
+{
+    angle=x;
+    display();
+}
+int main(int argc, char *argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_DEPTH);
+    glutCreateWindow("week03_mouse_rotate");
+    glutDisplayFunc(display);
+    glutMouseFunc(mouse);
+    glutMotionFunc(motion);/// motion的細節
+
+    glutMainLoop();
+}
