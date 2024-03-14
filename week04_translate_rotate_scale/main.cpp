1fb394306176567edb0d83fa8d741271e5a55d20
jimmyblack0112 <114201430+jimmyblack0112@users.noreply.github.com>
2024-03-14 16:30:35 +0800

    Add files via upload

---
+++ week04_translate_rotate_scale/main.cpp
@@ -0,0 +1,25 @@
+#include <GL/glut.h>
+#include <stdio.h>
+float angle = 0, oldX = 0;
+void display()
+{
+    glClearColor(1.0, 1.0, 0.9, 1.0); ///淡黃色背景
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    glPushMatrix();
+        glTranslatef(0.8, 0, 0); ///放到右邊去
+        glRotatef(angle++, 0, 0, 1); ///angle後面要++, 就會自己加角度, 自己轉
+        glScalef(0.3, 0.3, 0.3); ///小小的
+        glColor3f(0, 1, 0); ///綠色的
+        glutSolidTeapot(0.3); ///茶壺
+    glPopMatrix();
+    glutSwapBuffers();
+}
+int main(int argc, char*argv[])
+{
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_DEPTH);
+    glutCreateWindow("week04 keyboard mouse motion");
+    glutDisplayFunc(display);
+    glutIdleFunc(display); ///加這行,有空就重畫畫面
+    glutMainLoop();
+}
