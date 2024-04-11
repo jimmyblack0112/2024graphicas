be021a9109af1ab53008103a3b852a05be6c8bc3
jimmyblack0112 <114201430+jimmyblack0112@users.noreply.github.com>
2024-04-11 16:47:10 +0800

    week08

---
+++ week08-1_glm_gandam/main.cpp
@@ -0,0 +1,32 @@
+#include <GL/glut.h>
+
+#include "glm.h" ///5-a
+GLMmodel* pmodel = NULL; ///5-b
+void
+drawmodel(void) ///5-c
+{
+    if (!pmodel) {
+    pmodel = glmReadOBJ("data/gundam.obj");
+    if (!pmodel) exit(0);
+    glmUnitize(pmodel);
+    glmFacetNormals(pmodel);
+    glmVertexNormals(pmodel, 90.0);
+}
+        glmDraw(pmodel, GLM_SMOOTH | GLM_MATERIAL);
+}
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    drawmodel();///glutSolidTeapot(0.3);
+    glutSwapBuffers();
+}
+
+int main(int argc, char*argv[])
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_DEPTH);
+    glutCreateWindow("week08 gundam");
+    glutDisplayFunc(display);
+    glutMainLoop();
+}
